5bd3b227da12967f95e0be7a5d373191b0a3233f
한송희 <98_han@naver.com>
2022-09-09 23:21:54 +0900

    Feat : viewset을 이용한 Order CRUD 구현
    - 데이터의 직렬화를 위해 model seralizer 생성
    - viewset을 이용한 simple crud 구현
    - router를 이용한 url 연결

---
+++ apps/orders/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from .views import OrderViewSet
+
+router = DefaultRouter()
+router.register('orders', viewset=OrderViewSet, basename='order')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Order
+from .serializers import OrderSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
