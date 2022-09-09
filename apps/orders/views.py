7d77557a7042eb1311d9c54313d8d11d2641015e
한송희 <98_han@naver.com>
2022-09-09 23:41:04 +0900

    Feat : 주문 내역 검색
    - delivery_cost와 coupon_id를 제외한 order의 모든 필드로 검색 가능

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from rest_framework import viewsets
 
 from .models import Order
@@ -6,4 +8,15 @@ from .serializers import OrderSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        queryset = Order.objects.all()
+        q = self.request.GET.get('q', '')
+        if q:
+            queryset = queryset.filter(
+                Q(id__icontains=q) | Q(date__icontains=q) | Q(pay_state__icontains=q) | Q(quantity__icontains=q) |
+                Q(price__icontains=q) | Q(buyr_name__icontains=q) | Q(buyr_city__icontains=q) |
+                Q(buyr_country__icontains=q) | Q(buyr_zipx__icontains=q) | Q(vccode__icontains=q) |
+                Q(delivery_num__icontains=q) | Q(start_at__icontains=q) | Q(end_at__icontains=q)
+            )
+        return queryset
