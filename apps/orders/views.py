646a121eabfbae570eebdcb6b6a62105635fd0df
한송희 <98_han@naver.com>
2022-09-10 20:24:39 +0900

    Feat : django filter를 이용한 시작일자, 종료일자에 따른 필터 기능 구현
    - DateField 특성으로 기존 q객체를 이용한 검색 기능 작동하지 않음을 확인
    - 날짜 검색을 위해 django filter를 이용하여 필터 기능 구현

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,9 +1,19 @@
 from django.db.models import Q
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
 from rest_framework import viewsets
 
 from .models import Order
 from .serializers import OrderSerializer
+
+
+class DateFilter(filters.FilterSet):
+    date = filters.DateFromToRangeFilter()
+
+    class Meta:
+        model = Order
+        fields = ['start_at', 'end_at']
 
 
 class OrderViewSet(viewsets.ModelViewSet):
@@ -17,6 +27,9 @@ class OrderViewSet(viewsets.ModelViewSet):
                 Q(id__icontains=q) | Q(date__icontains=q) | Q(pay_state__icontains=q) | Q(quantity__icontains=q) |
                 Q(price__icontains=q) | Q(buyr_name__icontains=q) | Q(buyr_city__icontains=q) |
                 Q(buyr_country__icontains=q) | Q(buyr_zipx__icontains=q) | Q(vccode__icontains=q) |
-                Q(delivery_num__icontains=q) | Q(start_at__icontains=q) | Q(end_at__icontains=q)
+                Q(delivery_num__icontains=q)
             )
         return queryset
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = DateFilter
