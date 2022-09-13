7039c128aa25852d39b3818a75b60c5fef5df771
한송희 <98_han@naver.com>
2022-09-13 21:10:33 +0900

    Fix : 매매기준 환율  type error 해결
    - 매매기준 환율이 str 타입으로 되어 있어 type error 발생
    - str 타입에서 float 타입으로 형 변환

--- apps/orders/utils.py
+++ apps/orders/utils.py
@@ -12,5 +12,8 @@ def get_current_rate_of_exchange(request):
     }
 
     response = requests.get(url=url, params=params).json()
-    current_rate_of_exchange = response[-1]['deal_bas_r']
+    deal_base_r = response[-1]['deal_bas_r']
+    replace_comma_deal_base_r = deal_base_r.replace(",", "")
+
+    current_rate_of_exchange = float(replace_comma_deal_base_r)
     return current_rate_of_exchange
