886ccb6f6ac6cd05d44cb8c252fb382b0c9e8581
한송희 <98_han@naver.com>
2022-09-14 12:53:53 +0900

    Fix : 모델링으로 인한 conflict 해결

--- apps/coupons/models.py
+++ apps/coupons/models.py
@@ -8,35 +8,13 @@ class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
-    # discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     active = models.BooleanField(default=True)
-    num_available = models.PositiveIntegerField(null=True)
-    num_used = models.IntegerField(default=0)
 
     class Meta:
         db_table = 'coupon'
 
     def __str__(self):
         return self.code
-
-    # def can_use(self):
-    #     active = True
-    #
-    #     # if self.active == False:
-    #     #     active = False
-    #
-    #     if self.num_used >= self.num_available != 0:
-    #         active = False
-    #
-    #     return active
-    #
-    # def use(self):
-    #     self.num_used = self.num_used + 1
-    #
-    #     if self.num_used == self.num_available:
-    #         self.active = False
-    #
-    #     self.save()
 
 
 class CouponType(models.Model):
@@ -46,7 +24,6 @@ class CouponType(models.Model):
     % 할인 : 2
     """
     type = models.PositiveIntegerField()
-    # discount = models.PositiveIntegerField()
 
     class Meta:
         db_table = 'coupon_type'
@@ -57,7 +34,6 @@ class ClaimedCoupon(models.Model):
     redeemed = models.DateTimeField(auto_now_add=True)
     coupon = models.ForeignKey('Coupon', on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
 
     class Meta:
         db_table = 'claimed_coupon'
