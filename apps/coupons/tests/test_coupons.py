f8cb6727a738b82c0fc6b0294077f69c5e3b36a3
한송희 <98_han@naver.com>
2022-09-15 12:07:05 +0900

    Test : 쿠폰 CRUD API TESTCASE 작성 및 __init__ 파일 추가

--- apps/coupons/tests/test_coupons.py
+++ apps/coupons/tests/test_coupons.py
@@ -4,30 +4,18 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from apps.coupons.models import Coupon, CouponType
-from apps.deliveires.models import CountryCode, DeliveryCost
+
 
 class CouponTestCase(APITestCase):
     """ Coupon Viewset action(list, create, retrieve ,update,partial_update, destory)이 정상 작동하는지 확인하기 위한 테스트 """
 
     def setUp(self):
-        coupon_type_1 = CouponType.objects.create(type=1)
-        coupon_type_2 = CouponType.objects.create(type=2)
-        coupon_type_3 = CouponType.objects.create(type=3)
-
+        coupon_instance = CouponType.objects.create(type=1)
+        CouponType.objects.create(type=2)
+        CouponType.objects.create(type=3)
         Coupon.objects.create(
             code= "TESTCOUPONCODE", valid_from="2022-09-14 04:29:00", valid_to="2022-09-20 04:29:00", value=3000,
-            active=True, type=coupon_type_1)
-        Coupon.objects.create(
-            code= "TESTCOUPONCODE", valid_from="2022-09-14 04:29:00", valid_to="2022-09-20 04:29:00", value=3000,
-            active=True, type=coupon_type_2)
-        Coupon.objects.create(
-            code= "TESTCOUPONCODE", valid_from="2022-09-14 04:29:00", valid_to="2022-09-20 04:29:00", value=3000,
-            active=True, type=coupon_type_3)
-
-        CountryCode.objects.create(country_idx=221, country_code="US", country_dcode="+1", country_name="USA")
-        DeliveryCost.objects.create(id=1, quantity=1, USA=33370)
-        DeliveryCost.objects.create(id=1, quantity=2, USA=42620)
-        DeliveryCost.objects.create(id=1, quantity=3, USA=65750)
+            active=True, type=coupon_instance)
 
     def test_create_coupon(self):
         data = {
@@ -45,7 +33,6 @@ class CouponTestCase(APITestCase):
     def test_get_couponlist(self):
         response = self.client.get(reverse('coupon-list'))
         self.assertEquals(response.status_code, status.HTTP_200_OK)
-
 
     def test_get_coupon_retrieve(self):
         response = self.client.get(reverse('coupon-detail', kwargs={'pk': 1}))
